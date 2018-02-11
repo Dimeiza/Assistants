30cbac11176a14990b5b1edd8e90c321148ca7b3
dimeiza <dimeiza@hotmail.com>
2018-02-11 12:53:27 +0900

    Add setupscript for Alexa

---
+++ AlexaSetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Enter informations for Alexa"
+read -r -p "Your Client ID:" clientid
+echo ""
+read -r -p "Your Client Secret:" clientsecret
+echo ""
+read -r -p "your product id:" productid
+echo ""
+
+serialno=$productid-$(date +%Y%m%d%H%M%S )
+
+cat << EOF > "config.txt"
+CLIENT_SECRET="$clientid"
+#NOTE: The Device Serial Number can be any unique number
+DEVICE_SERIAL_NUMBER="$serialno"
+CLIENT_ID="$clientsecret"
+PRODUCT_ID="$productid"
+EOF
+
+wget https://raw.githubusercontent.com/Dimeiza/avs-device-sdk/master/tools/RaspberryPi/setup.sh
+chmod 700 ./setup.sh
+
+./setup.sh config.txt
--- AssistantSetup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/Dimeiza/avs-device-sdk/master/tools/RaspberryPi/setup.sh
-chmod 700 ./setup.sh
-
-./setup.sh
