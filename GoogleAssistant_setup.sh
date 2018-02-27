ff37b8e1a7e0b2aaba433bece3f7b06ea6d55281
Dimeiza <dimeiza@hotmail.com>
2018-02-27 20:56:38 +0900

    Update GoogleAssistant_setup.sh
    Fix: add wheel installation

--- GoogleAssistant_setup.sh
+++ GoogleAssistant_setup.sh
@@ -18,7 +18,7 @@ sudo apt-get -y install python3-dev python3-venv # Use python3.4-venv if the pac
 sudo apt-get -y install portaudio19-dev libffi-dev libssl-dev
 
 python3 -m venv env
-env/bin/python -m pip install --upgrade pip setuptools
+env/bin/python -m pip install --upgrade pip setuptools wheel
 source env/bin/activate
 
 python -m pip install --upgrade google-assistant-library
